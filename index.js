27a3c6424c40fe26058bee8c81e48a3d73dc9ae8
binding1 <jaey.pro@gmail.com>
2022-11-05 04:37:15 +0000

    refactored code as an object for OOP

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-
-// Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
+function newTask(title, description) { //object with task and it's properties
+  const task = {
+    title: title, //title of said task
+    description: description, //description of said task
+    complete: false, //status of if the task has been completed
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
+    logState: () => {
+      console.log(`${task.title} has ${task.complete ? " " : " not "} been completed`) // Print the state of a task to the console in a nice readable way
+    },
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+    markCompleted: () => {
+      this.complete = true; // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+    }
+  };
+  return task;
+};
+
 
 // DRIVER CODE BELOW
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
